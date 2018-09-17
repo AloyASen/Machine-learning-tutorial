58f16af267c9681aaf6b1f04afc69715b2d05217
aloysen <aloysen951@gmail.com>
2018-09-17 13:29:20 +0530

    upload changes to github ml

---
+++ day5_nltk/stemming.py
@@ -0,0 +1,20 @@
+#finding the word root may help to find its meanings
+
+from nltk.stem import PorterStemmer
+from nltk.tokenize import sent_tokenize, word_tokenize
+
+ps = PorterStemmer()
+
+# examples
+
+#case 1 -- only words
+example_words = ["python","pythoner","pythoning","pythoned","pythonly"]
+for w in example_words:
+    print(ps.stem(w))
+
+#case 2 -- for sentences
+new_text = "It is important to by very pythonly while you are pythoning with python. All pythoners have pythoned poorly at least once."
+words = word_tokenize(new_text)
+
+for w in words:
+    print(ps.stem(w))
